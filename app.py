27b0cb0e942346a35d2d589d3a7a126143926fd0
Ziam-am <127385299+Ziam-am@users.noreply.github.com>
2023-06-02 17:57:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,32 @@
 from process import preparation, generate_response
 from flask import Flask, render_template, request
 
+from flask_ngrok import run_with_ngrok
+
 # download nltk
 preparation()
 
 #Start Chatbot
 app = Flask(__name__)
 
+#routing halaman utama
 @app.route("/")
 def home():
     return render_template("index.html")
 
+#routing aplikasi
+@app.route("/chatbot")
+def chatbot():
+    return render_template('chatbot.html')
+
+#routing chatbot
 @app.route("/get")
 def get_bot_response():
     user_input = str(request.args.get('msg'))
     result = generate_response(user_input)
     return result
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    #app.run(debug=True)
+    run_with_ngrok(app)
+	app.run()
